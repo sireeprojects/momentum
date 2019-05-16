5f81144c15081956f4ebbe50a533292b8aeb1b35
root <root@localhost.localdomain>
2019-05-16 14:47:57 -0400

    updates

--- projects/iProxy/iProxy.cpp
+++ projects/iProxy/iProxy.cpp
@@ -2,7 +2,19 @@
 
 namespace ethTester {
 
-iProxy::iProxy(unsigned int id, string name, string dataPlaneSocket, unsigned int txBufSize, unsigned int rxBufSize) {
+int swapDataPlane(iProxy *dest, iProxy *src) {
+    return 0;
+}
+
+int connectToControlPlane(string path) {
+    return 0;
+}
+
+int closeControlPlane() {
+    return 0;
+}
+
+iProxy::iProxy(unsigned int id, string name, string dataPlaneSocket, unsigned int txBufSize, unsigned int rxBufSize) { 
 }
 
 iProxy::~iProxy() {
@@ -12,10 +24,39 @@ unsigned int iProxy::txWorker(unsigned int numElemsAvail) {
     return 0;
 }
 
-void iProxy::rxWorker(unsigned int *data, unsigned char eo) {
+void iProxy::rxWorker(unsigned int *elemData, unsigned char eom) {
 }
 
-void iProxy::printFrame() {
+void iProxy::reset(iProxyResetType type) {
+}
+
+void iProxy::configure(iProxyFeature feature, unsigned int value) {
+}
+
+int iProxy::connectToDataPlane(string path) {
+    return 0;
+}
+
+int iProxy::setDataPlaneSocketBufSize(unsigned int txSize, unsigned int rxSize) {
+    return 0;
+}
+
+int iProxy::closeDataPlane() {
+    return 0;
+}
+
+void iProxy::printFrame(iProxyDirection dir, unsigned char *data, unsigned int len) {
+}
+
+void iProxy::enableAutoEot() {
+}
+
+char *iProxy::readableFS(double size, char *buf) {
+    return buf;
+}
+
+int iProxy::fdSetBlocking(int fd, int blocking) {
+    return 0;
 }
 
 }
--- projects/iProxy/iProxy.h
+++ projects/iProxy/iProxy.h
@@ -6,23 +6,48 @@
 #include <string.h>
 
 namespace ethTester {
+using namespace std;
+class iProxy;
 
-using namespace std;    
+typedef enum {TX,RX} iProxyDirection; 
+typedef enum {SOFT,HARD} iProxyResetType; 
+typedef enum {CONNECT, DISCONNECT} cpCommand;
+typedef enum {FEAT1,FEAT2} iProxyFeature; 
 
-typedef enum {
-    TX, 
-    RX
-} iProxyDirection;
+// used to swap the dataplane descriptors
+// during dynamic reset or speed change 
+int swapDataPlane(iProxy *dest, iProxy *src);
+
+// create a control plane between this proxy instance and adaptor
+// control and status will be exchanged in this plane
+// This should be the first called made from the porxy/bfm before any other action
+int connectToControlPlane(string path);
+
+// notify the adaptor that the test has ended
+// adaptor uses this notification to release all the 
+// resources that was reserved for the test. 
+// this should be called just before c_stop_sim
+int closeControlPlane();
+
+// file descriptor of the control plane
+int cpSock = -1;
 
 class iProxy {
+
 public:
+
     // ctor
     iProxy(
-        unsigned int id,
-        string name,
-        string dataPlaneSocket,
-        unsigned int txBufSize,
-        unsigned int rxBufSize);
+        // instance number of the bfm 
+        unsigned int id,         
+        // a string identified that will be used in messages
+        string name,             
+        // rtl path to the bfm that this instance will be connected to
+        string dataPlaneSocket,  
+        // size of the transmit dataplane
+        unsigned int txBufSize,  
+        // size of the receive datapath
+        unsigned int rxBufSize); 
 
     // dtor
     ~iProxy();
@@ -33,15 +58,48 @@ public:
 
     // receive frames from HW side and write into
     // datapath socket leading to the adaptor
-    void rxWorker(unsigned int *data, unsigned char eom);
+    void rxWorker(unsigned int *elemData, unsigned char eom);
+
+    // soft: retains all the statistics and updates continue when
+    //       resumed shud be used in multi reset and speed change tests
+    // hard: bring the proxy instance to its default state
+    void reset(iProxyResetType type);
+
+    void configure(iProxyFeature feature, unsigned int value);
 
 private:
-    // print the bytes of a frame in hex format 
-    // for both Tx and Rx frames
-    void printFrame();
 
+    // create a dataplane between this proxy instance and adaptor
+    // frames will be exchanged in the plane
+    int connectToDataPlane(string path);
 
+    // reserve buffer for both dataplane and control plane transfers
+    int setDataPlaneSocketBufSize(unsigned int txSize, unsigned int rxSize);
+
+    // notify adaptor that this proxy instance is going to be 
+    // deleted and hence release the resources that was reserved 
+    // by this proxy instance. will be called before a proxy instance 
+    // is deleted
+    int closeDataPlane();
+
+    // print the bytes of a frame in hex 
+    // format for both Tx and Rx frames
+    // available in debug build
+    void printFrame(iProxyDirection dir, unsigned char *data, unsigned int len);
+
+    // can be used in loopback test. an end of signal will be
+    // sent to bfm when the number of frames matches on both 
+    // tx and rx sides. the num transaction should also be set
+    // available in debug build
+    void enableAutoEot();
+
+    // convert size in bytes to filesystem format, KB/MB/GB etc
+    char *readableFS(double size, char *buf);
+
+    // use to put the dataplane in blocking or nonblocking mode
+    int fdSetBlocking(int fd, int blocking);
 };
+
 }
 
 #endif
