39a865e082c185b3c83f480ba2a67cb0be13c5f9
root <root@localhost.localdomain>
2019-07-19 19:56:19 -0400

    updates

--- projects/hwgen/defines.svh
+++
@@ -1,4 +0,0 @@
-`define DW      64
-`define CW      6
-`define DEEP    32
-`define MW      256
--- projects/hwgen/run.tcl
+++
@@ -1,4 +0,0 @@
-database -open -shm waves.shm
-probe -create -database waves.shm top -depth all -all -memories
-run 1000ns
-exit
