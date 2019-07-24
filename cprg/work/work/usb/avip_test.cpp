bec593b288ebfbf7c9a3aab985a6d3f3130cc6a2
root <root@localhost.localdomain>
2019-07-24 17:58:55 +0530

    updates

--- cprg/work/work/usb/avip_test.cpp
+++ cprg/work/work/usb/avip_test.cpp
@@ -9,26 +9,28 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
    usboeSocket sock;
    sock.init_server(8888);
    sock.wait_for_client();
    char data[1024];
    unsigned int DL = 8;
 
-   while (1)
-   {
+   while (1) {
       int len = sock.read_data (data);
 
       printf ("Data Received from Remote USB Client -> ");
-      for (int i=0 ;i < DL; i++) 
+
+      for (int i=0 ;i < DL; i++) {
          printf("%02x;",data[i]);
+      }
+
       printf("\n");
 
       if (data[2]==0x29)
          break;
    }
+
    printf ("Closing Socket connection\n");
    sock.close_server ();
    return 0;
